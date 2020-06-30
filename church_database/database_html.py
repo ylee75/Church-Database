41f205f00645b4eb559c063e82175ed759c25d13
Jeffrey Liew <jliew626@utexas.edu>
2020-06-30 03:42:04 -0500

    New Text File
    Added so that python creates a textbox to copy paste the HTML code

--- church_database/database_html.py
+++ church_database/database_html.py
@@ -1,20 +1,30 @@
 import openpyxl
 import emoji
+import sys
 
 # Setting up Excel Document
 wb = openpyxl.load_workbook('church_database.xlsx')
 wb.sheetnames
 sheet = wb['Sheet1']
 
+# /Users/jeffreyliew/Documents/GitHub/Church-Database/church_database/database_html.py > /Users/jeffreyliew/Documents/GitHub/Church-Database/church_database/HTML.txt
+sys.stout = open('HTML.txt', 'w')
+
+# Setting up HTML
+setUp = "\n<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Spartan\">  <!-- Access to Google Fonts API --> \n<div style=\"overflow-x:auto;\">\n *: College Ministry Available (Check to filter) <input type=\"checkbox\" id=\"myCheck\" onclick=\"myFunction()\">\n<table id=\"myTable\">\n  <tr class=\"header\">\n    <th style=\"width:16%;\" onclick=\"sortTable(0)\">Name</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(1)\">Sunday Worship Time</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(2)\">Address</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(3)\">Distance (miles)</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(4)\">Denomination</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(5)\">Contact Info</th>\n  </tr>"
+print(setUp)
+
 # Initalizing the first value
-name = "Begin"
+notEmpty = 1
 i = 2
 emoji = ('\U0001F392')
+
 # Run through each row
-while(len(name) != 0):
+while(notEmpty):
     # Read the row and store in appropiate variables
     print()
     name = sheet['A{}'.format(i)].value
+    adds = sheet['B{}'.format(i)].value
     dist = sheet['C{}'.format(i)].value
     deno = sheet['D{}'.format(i)].value
     link = sheet['E{}'.format(i)].value
@@ -22,21 +32,31 @@ while(len(name) != 0):
     cont = sheet['H{}'.format(i)].value
     time = sheet['I{}'.format(i)].value
 
+
+
+
+    # Print out HTML code
+    print('    <tr>')
     # Check for college ministry
     if(cMin == 'Y'):
         cMin = emoji
-    elif(cMin == 'N'):
-        cMin = ''
+        print('        <td><a href={} target="blank">{}</a>{}</td>'.format(link, name,cMin))
 
-    # Print out HTML code
-    print('<tr>')
-    print('    <td><a href={} target="blank">{}</a>{}</td>'.format(link, name,cMin))
-    print('    <td>{}</td>'.format(time))
-    print('    <td>{}</td>'.format(dist))
-    print('    <td>{}</td>'.format(deno))
-    print('    <td>{}</td>'.format(cont))
-    print('</tr>')
+    else:
+        print('        <td><a href={} target="blank">{}</a></td>'.format(link, name))
+    print('        <td>{}</td>'.format(time))
+    print('        <td>{}</td>'.format(adds))
+    print('        <td>{}</td>'.format(dist))
+    print('        <td>{}</td>'.format(deno))
+    print('        <td>{}</td>'.format(cont))
+    print('    </tr>')
 
-    i = i +1
-    if(i == 4):
-        name = ""
+    i = i + 1
+    name = sheet['A{}'.format(i)].value
+    if not name:
+        notEmpty = 0
+
+print()
+print("</table>\n</div>\n")
+
+sys.stdout.close()
